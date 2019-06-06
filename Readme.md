540a094b5ba7a6d4d3c49f03b00ea347ba126da0
Kamil Stopyra <kamil.stopyra95@gmail.com>
2019-06-06 14:23:31 +0200

    Init frontend commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+## Run
+
+### `npm install`
+### `npm start`
+
+## Endpoints
+
+##### Upload file
+
+[http://localhost:3000/upload](http://localhost:3000/upload) ->  send post request with file to REST server
+[http://localhost:3000/lessons](http://localhost:3000/upload) ->  main calendar component
+
+
+
---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    document.title = "Scheduling Supporter"
+  }
+  redirectToUpload = () => {
+    this.props.history.push(`/upload`)
+  }
+  render() {
+    return (
+        <div className="header">
+          <div style={{padding: "50px"}}>
+          <div className="row"><div className="col-xs-12">
+              <button className="btn btn-success" onClick={this.redirectToUpload}>Upload Schedule</button>
+          </div></div>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default App;
